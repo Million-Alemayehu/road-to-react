3e7e23cfc5c6ed32b3ca2379a46286a5bee3f38f
Million-Alemayehu <millionalemayehu@gmail.com>
2025-11-27 19:10:37 +0300

    creating my first react

---
+++ react-stories/src/App.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const title = 'World';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello {title} Its Million</h1>
+    </div>
+  );
+}
+
+export default App
