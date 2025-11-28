e9c5e57dcaf0a0cf806a8c7e9eb4f2952ffbafe2
Million-Alemayehu <millionalemayehu@gmail.com>
2025-11-28 16:35:18 +0300

    react list and array

--- react-stories/src/App.jsx
+++ react-stories/src/App.jsx
@@ -1,13 +1,45 @@
 import * as React from 'react';
 
-const title = 'World';
+const list =[ {
+    title: 'react',
+    url: 'httpps://reactrjs.org/',
+    author:'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'redux',
+    url: 'httpps://redux.js.org/',
+    author:'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 
 function App() {
+ 
 
   return (
     <div>
-      <h1>Hello {title} Its Million</h1>
+      <h1>My Hacker Stories</h1> 
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map(function (item) {
+          return <li key ={item.objectID}>{item.title}</li>;
+        })}
+        
+      </ul>
     </div>
+
+
   );
 }
 
