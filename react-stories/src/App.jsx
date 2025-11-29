d78352733fcf8465ad1e3acad79fe6dc87f6cbf0
Million-Alemayehu <millionalemayehu@gmail.com>
2025-11-29 09:43:11 +0300

    react components

--- react-stories/src/App.jsx
+++ react-stories/src/App.jsx
@@ -1,46 +1,62 @@
 import * as React from 'react';
 
-const list =[ {
+const list = [
+  {
     title: 'react',
-    url: 'httpps://reactrjs.org/',
-    author:'Jordan Walke',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
     title: 'redux',
-    url: 'httpps://redux.js.org/',
-    author:'Dan Abramov, Andrew Clark',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
     points: 5,
     objectID: 1,
   },
 ];
 
-
 function App() {
- 
-
   return (
     <div>
-      <h1>My Hacker Stories</h1> 
+      <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map(function (item) {
-          return <li key ={item.objectID}>{item.title}</li>;
-        })}
-        
-      </ul>
+      <List />
     </div>
-
-
   );
 }
 
-export default App
+function List() {
+  return (
+    <ul>
+      {list.map((item) => (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>{' '}
+          <span>{item.author}</span>{' '}
+          <span>{item.num_comments}</span>{' '}
+          <span>{item.points}</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+export default App;
