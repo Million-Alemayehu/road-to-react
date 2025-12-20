752673d2f3068524efda03517a267f579f00248d
Million-Alemayehu <millionalemayehu@gmail.com>
2025-12-20 15:00:02 +0300

    final on react custom hook

--- react-stories/src/App.jsx
+++ react-stories/src/App.jsx
@@ -1,5 +1,16 @@
 import * as React from 'react';
 
+const useStorageState = (key, initialState) => {
+    const [value, setValue] = React.useState(
+      localStorage.getItem(key) || initialState
+  );
+
+    React.useEffect(() => {
+      localStorage.setItem(key, value);
+    }, [value, key]);
+    return [value, setValue];
+  };
+
 const App = () => {
   const stories =[
     { 
@@ -20,11 +31,12 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState(localStorage.getItem('search') || 'React');
-  
-  React.useEffect(() => {
-    localStorage.setItem('search', searchTerm);
-  }, [searchTerm]);
+  //const [searchTerm, setSearchTerm] = React.useState(localStorage.getItem('search') || 'React');
+
+  const [searchTerm, setSearchTerm] = useStorageState(
+      'React',
+      'search'
+    );
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
