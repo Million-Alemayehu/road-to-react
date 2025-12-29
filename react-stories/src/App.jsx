096c7297268226a493c0bf4003af42d3646aeb8c
Million-Alemayehu <millionalemayehu@gmail.com>
2025-12-29 16:58:05 +0300

    change on the reducer function and exchanged state updater function with dispatch function

--- react-stories/src/App.jsx
+++ react-stories/src/App.jsx
@@ -29,12 +29,32 @@ const getAsyncStories = () =>
 
 const storiesReducer = (state, action) => {
   switch(action.type){
-    case 'SET_STORY':
-      return action.payload;
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload, 
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
     case 'REMOVE_STORY':
-      return state.filter(
-        (story) => action.payload.objectID !== story.objectID
-      );
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
     default:
       throw new Error();
   }
@@ -60,23 +80,22 @@ const App = () => {
       
  const [stories, dispatchStories] = React.useReducer(
         storiesReducer,
-        []
+        {data:[], isLoading: false, isError: false}
       );
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
-    setIsLoading(true);
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
     getAsyncStories()
       .then((result) => {
         dispatchStories({
-          type: 'SET_STORY',
+          type: 'STORIES_FETCH_SUCCESS',
           payload: result.data.stories,
         });
-        setIsLoading(false);
       })
-      .catch(() => setIsError(true));
+      .catch(() => 
+        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+    );
   }, []);
 
   const handleRemoveStory = (item) => {
