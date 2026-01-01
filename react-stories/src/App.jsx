19b06733ec492363739970fb9ee61c5490eb096c
Million-Alemayehu <millionalemayehu@gmail.com>
2026-01-01 12:30:17 +0300

    data fatching in react

--- react-stories/src/App.jsx
+++ react-stories/src/App.jsx
@@ -1,32 +1,5 @@
 import * as React from 'react';
 
-const initiaStories =[
-  { 
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  { 
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(
-      () => resolve({data: {stories: initiaStories}}),
-      2000
-    )
-  );
-
 const storiesReducer = (state, action) => {
   switch(action.type){
     case 'STORIES_FETCH_INIT':
@@ -71,6 +44,8 @@ const useStorageState = (key, initialState) => {
 
     return [value, setValue];
 };
+//A
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState(
@@ -86,11 +61,12 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => 
