b3757b59640fac725b8e4979de360f70dcc9f070
Million-Alemayehu <millionalemayehu@gmail.com>
2026-01-09 17:16:46 +0300

    forms in react

--- react-stories/src/App.jsx
+++ react-stories/src/App.jsx
@@ -93,30 +93,37 @@ const App = () => {
       setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event) => {
       setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+      event.preventDefault();
   };
 
   return(
       <div>
         <h1>My Hacker Stories</h1>
 
-        <InputWithLabel
-          id="search"
-          value={searchTerm}
-          isFocused
-          onInputChange={handleSearchInput}
-        >
-          <strong>Search: </strong>
-        </InputWithLabel>
+        <SearchForm
+          searchTerm={searchTerm}
+          onSearchInput={handleSearchInput}
+          onSearchSubmit={handleSearchSubmit}
+        />
 
-        <button
-          type="button"
-          disabled={!searchTerm}
-          onClick={handleSearchSubmit}
-        >
-          Submit
-        </button>
+        {/**<form onSubmit={handleSearchSubmit}>
+          <InputWithLabel
+            id="search"
+            value={searchTerm}
+            isFocused
+            onInputChange={handleSearchInput}
+          >
+            <strong>Search: </strong>
+          </InputWithLabel>
+
+          <button type= "submit" disabled ={!searchTerm}>
+            Submit
+          </button>
+        </form>**/}
+        
 
         <hr />
 
@@ -188,6 +195,27 @@ const Item = ({item, onRemoveItem}) =>(
       </button>
     </span>
   </li>
+);
+
+const SearchForm = ({
+      searchTerm,
+      onSearchInput,
+      onSearchSubmit,
+}) => (
+    <form onSubmit={onSearchSubmit}>
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={onSearchInput}
+      >
+        <strong>Search: </strong>
+      </InputWithLabel>
+
+      <button type= "submit" disabled ={!searchTerm}>
+        Submit
+      </button>
+    </form>
 );
 
 export default App;
